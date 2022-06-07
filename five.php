a3b10d77612f8098f965b8735802d00fe328487d
NaimurRahman19403971 <91046074+NaimurRahman19403971@users.noreply.github.com>
2022-06-07 13:56:14 +0600

    Add files via upload

---
+++ five.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+function check($a,$b,$c){
+
+if($a>$b && $a>$c){
+
+	
+	echo ("The largest number is ")."".$a;
+
+}
+else if($b>$a && $b>$c){
+	echo ("The largest number is ")."".$b;
+}
+else{
+	echo ("The largest number is ")."".$c;
+}
+}
+
+check(490,50,51)
+
+
+
+
+
+
+
+?>
